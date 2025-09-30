4c2ae46e61c768a545ae4e979b346325b4c0f480
jcoon3 <jcoon3@fingerlakes.edu>
2025-09-30 19:31:37 +0000

    C_to_F_02.py take 2

--- convert_C_to_F_02.py
+++ convert_C_to_F_02.py
@@ -3,11 +3,11 @@
 # Description: A program that converts celcius to fahrenheit and vice versa
 
 # Prints a banner
-print("===== Temperature Converter =====")
+print("===== Temperature Converter =====\n")
 
 # Prints the menu options
 print("1. Convert from Celsius to Fahrenheit")
-print("2. Convert from Fahrenheit to Celsius")
+print("2. Convert from Fahrenheit to Celsius\n")
 
 # Variable declaration for the menu choice and temperature to be converted
 user_choice = int(input("Please choose from the above menu: "))
@@ -24,9 +24,9 @@ fahrenheit = temperature * 9/5 + 32
 # If statement to check if menu option 1 was selected and prints degrees Celsius to degrees Fahrenheit
 if user_choice == 1:
     
-    print(f"{temperature} degrees Celsius is {fahrenheit} degrees Fahrenheit")
+    print(f"\n{temperature} degrees Celsius is {fahrenheit} degrees Fahrenheit")
 
 # Elif clause that checks if menu option 2 was chosen and prints degrees Fahrenheit to degrees Celsius    
 elif user_choice == 2:
     
-   print(f"{temperature} degrees Fahrenheit is {celsius} degrees Fahrenheit")
+   print(f"\n{temperature} degrees Fahrenheit is {celsius} degrees Fahrenheit")
